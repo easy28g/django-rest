2071b31d3c2eb6d71e78878f0c0be45568df599b
azamat <azamat27071998@gmail.com>
2024-03-27 21:37:30 +0600

    RetrieveUpdateDestroyAPIView

--- rest_project_1/rest_project_1/urls.py
+++ rest_project_1/rest_project_1/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from women.views import WomenAPIView, WomenListAPIView
+from women.views import WomenListAPIView, WomenAPIUpdate, WomenAPIDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api/v1/womenlist/', WomenAPIView.as_view()),
     # path('api/v1/womenlist/<int:pk>/', WomenAPIView.as_view()),
     path('api/v1/womenlist/', WomenListAPIView.as_view()),
-    path('api/v1/womenlist/<int:pk>/', WomenListAPIView.as_view()),
+    # path('api/v1/womenlist/<int:pk>/', WomenListAPIView.as_view()),
+    path('api/v1/womenlist/<int:pk>/', WomenAPIUpdate.as_view()),
+    path('api/v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
 ]
--- rest_project_1/women/views.py
+++ rest_project_1/women/views.py
@@ -11,29 +11,41 @@ class WomenListAPIView(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
     
+class WomenAPIUpdate(generics.UpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
     
-class WomenAPIView(APIView):
+class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+  
+  
+#Medium  
     
-    def get(self, request):
-        w = Women.objects.all()
-        return Response({'posts': WomenSerializer(w, many=True).data})
+# class WomenAPIView(APIView):
     
-    def post(self, request):
-        serializer = WomenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'post': serializer.data})
+#     def get(self, request):
+#         w = Women.objects.all()
+#         return Response({'posts': WomenSerializer(w, many=True).data})
+    
+#     def post(self, request):
+#         serializer = WomenSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({'post': serializer.data})
     
     
-    def delete(self, request, pk):
-        try:
-            instance = Women.objects.get(pk=pk)
-        except Women.DoesNotExist:
-            return Response({"error": "Object does not exist"})
+#     def delete(self, request, pk):
+#         try:
+#             instance = Women.objects.get(pk=pk)
+#         except Women.DoesNotExist:
+#             return Response({"error": "Object does not exist"})
         
-        serializer = WomenSerializer(data=request.data)
-        serializer.delete(instance)
-        return Response({"success": "Object deleted successfully"})
+#         serializer = WomenSerializer(data=request.data)
+#         serializer.delete(instance)
+#         return Response({"success": "Object deleted successfully"})
+
+#Hard
 
 # class WomenAPIView(generics.ListAPIView):
 #     queryset = Women.objects.all()
