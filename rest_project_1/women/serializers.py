f648d502d63be492bdcc8db2f2c114a964a98337
azamat <azamat27071998@gmail.com>
2024-03-27 19:40:19 +0600

    serializer

--- rest_project_1/women/serializers.py
+++ rest_project_1/women/serializers.py
@@ -4,31 +4,35 @@ from .models import Women
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 
-class WomenModel:
-    def __init__(self, title, content):
-        self.title = title
-        self.content = content
+# class WomenModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
         
     
 class WomenSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
     
     
-def encode():
-    model = WomenModel('Angelina Jolie', 'Angelina Jolie')   
-    model_sr = WomenSerializer(model) 
-    print(model_sr.data, type(model_sr.data), sep='\n')
-    json = JSONRenderer().render(model_sr.data)
-    print(json)
+# def encode():
+#     model = WomenModel('Angelina Jolie', 'Angelina Jolie')   
+#     model_sr = WomenSerializer(model) 
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
     
 
-def decode():
-    stream = io.BytesIO(b'{"title":"Angelina Jolie","content":"Angelina Jolie"}')
-    data = JSONParser().parse(stream)
-    serializer = WomenSerializer(data=data)
-    serializer.is_valid()
-    print(serializer.validated_data)
+# def decode():
+#     stream = io.BytesIO(b'{"title":"Angelina Jolie","content":"Angelina Jolie"}')
+#     data = JSONParser().parse(stream)
+#     serializer = WomenSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
 
 # class WomenSerializer(serializers.ModelSerializer):
 #     class Meta:
