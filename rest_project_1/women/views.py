30cbc50a262a5c9918bf2ee4248ac2c492205674
azamat <azamat27071998@gmail.com>
2024-03-27 17:20:51 +0600

    women app

---
+++ rest_project_1/women/views.py
@@ -0,0 +1,7 @@
+from rest_framework import generics
+from .models import Women
+from .serializers import WomenSerializer
+
+class WomenAPIView(generics.ListAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
